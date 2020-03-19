65186613b77dee467686dad48360ffa98d4c3622
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-03-19 23:47:24 +0530

    Removed menu component

--- screens/Prevention.js
+++ screens/Prevention.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Header from "../components/Header";
-import Menu from "../components/Menu";
 
 export default function App() {
   return (
