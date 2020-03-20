9d902bd7152650debdba68b6226a22f0485f7887
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-03-21 01:08:42 +0530

    Added sources and contact info

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-import { globalStyles } from "../styles/global";
 
 export default function ViewBody(props) {
   return (
     <ScrollView>
-        <Image></Image>
+      <View style={styles.container}>
+        <Image
+          style={styles.map}
+          source={{
+            uri:
+              "https://cdn.technologyreview.com/i/images/screen-shot-2020-03-06-at-14.59.42.png?sw=2500&cx=0&cy=0&cw=1127&ch=989"
+          }}
+        />
+      </View>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "center",
+    alignContent: "center"
+  },
+  map: {
+    flex: 1,
+    resizeMode: "stretch",
+    height: 300
+  }
+});
--- screens/About.js
+++ screens/About.js
@@ -1,14 +1,36 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import { FontAwesome } from "@expo/vector-icons";
 import ViewHeader from "../components/ViewHeader";
+import { globalStyles } from "../styles/global";
 
 export default function About() {
-  const contentList = [
-    { key: "1", content: "Cough" },
-    { key: "2", content: "Tiredness" },
-    { key: "3", content: "Fever" },
-    { key: "4", content: "Difficulty in breathing" }
-  ];
+  async function _handleOpenWHOWebBrowser() {
+    await WebBrowser.openBrowserAsync(
+      "https://www.who.int/emergencies/diseases/novel-coronavirus-2019"
+    );
+  }
+  async function _handleOpenMITWebBrowser() {
+    await WebBrowser.openBrowserAsync(
+      "https://www.technologyreview.com/s/615330/best-worst-coronavirus-dashboards/"
+    );
+  }
+  async function _handleLinkedinWebBrowser() {
+    await WebBrowser.openBrowserAsync(
+      "https://www.technologyreview.com/s/615330/best-worst-coronavirus-dashboards/"
+    );
+  }
+  async function _handleEmailWebBrowser() {
+    await WebBrowser.openBrowserAsync(
+      "https://www.technologyreview.com/s/615330/best-worst-coronavirus-dashboards/"
+    );
+  }
+  async function _handleGithubWebBrowser() {
+    await WebBrowser.openBrowserAsync(
+      "https://www.technologyreview.com/s/615330/best-worst-coronavirus-dashboards/"
+    );
+  }
   return (
     <View style={styles.container}>
       <ViewHeader
@@ -16,9 +38,48 @@ export default function About() {
         color="#f7b731"
         subtitle="Hi, I am Abhishek, independent fullstack developer. I am a student currently studying computer science."
       />
-      <Text>
-        
-      </Text>
+      <View style={styles.sources}>
+        <Text style={[styles.subtitle, globalStyles.text]}>Sources:</Text>
+        <Button
+          title="World Health Organization"
+          color="#333"
+          onPress={_handleOpenWHOWebBrowser}
+          style={styles.button}
+        />
+        <Button
+          title="MIT Technology review"
+          color="#333"
+          onPress={_handleOpenMITWebBrowser}
+          style={styles.button}
+        />
+      </View>
+      <View style={styles.sources}>
+        <Text style={[styles.subtitle, globalStyles.text]}>Contact:</Text>
+        <FontAwesome.Button
+          name="linkedin"
+          color="#333"
+          onPress={_handleLinkedinWebBrowser}
+          style={styles.button}
+        >
+          linkedin
+        </FontAwesome.Button>
+        <FontAwesome.Button
+          name="envelope-o"
+          backgroundColor="#333"
+          onPress={_handleEmailWebBrowser}
+          style={styles.button}
+        >
+          Email
+        </FontAwesome.Button>
+        <FontAwesome.Button
+          name="github"
+          backgroundColor="#333"
+          onPress={_handleGithubWebBrowser}
+          style={styles.button}
+        >
+          Github
+        </FontAwesome.Button>
+      </View>
     </View>
   );
 }
@@ -27,5 +88,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff" // For debug
+  },
+  sources: {
+    flex: 1,
+    padding: 15,
+    justifyContent: "space-around",
+    backgroundColor: "#ecf0f1",
+    margin: 10,
+    borderRadius: 10
+  },
+  subtitle: {
+    fontSize: 18
+  },
+  button: {
+    textAlign: "center"
   }
 });
