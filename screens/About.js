de497419b3eef0730b1a60cfb0ae087067c4e728
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-03-21 09:51:42 +0530

    Final commit v0.2

--- screens/About.js
+++ screens/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import { FontAwesome } from "@expo/vector-icons";
 import ViewHeader from "../components/ViewHeader";
